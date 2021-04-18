bd06183778b09dc9dddfab1687136004042d3d5b
z-kollath <64993207+z-kollath@users.noreply.github.com>
2021-04-18 22:25:39 +0200

    210418

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
